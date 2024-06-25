a8aa15bbff5bbb0cb3b63ae3c57961cfa1f28a9d
Lucas Meier <lucas@cronokirby.com>
2024-06-25 01:05:31 -0700

    Add basic crate structure

---
+++ crates/genbo-lib/src/lib.rs
@@ -0,0 +1,2 @@
+/// A constant used only for testing linkage between our workspace crates.
+pub const TEST: &'static str = "Hello World!";
---
+++ genbo/src/main.rs
@@ -0,0 +1,5 @@
+use genbo_lib::TEST;
+
+fn main() {
+    println!("{}", TEST);
+}
